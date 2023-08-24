82e15dc494fac28ebcee3bb8969795db23c5ba1b
Henrique Schaan <102075535+henriqueschaan@users.noreply.github.com>
2023-08-23 21:17:43 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={increment}>
+          Clique aqui
+        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Você clicou {count} vezes.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
